923f96ea24422bc2a1aad5f1fa4edd079e31475d
Javenhun733r <andrij.babyuk@gmail.com>
2025-09-25 15:26:05 +0300

    Code refactoring

--- back/src/config/s3.js
+++ back/src/config/s3.js
@@ -1,13 +1,29 @@
 import { S3Client } from "@aws-sdk/client-s3";
-import dotenv from "dotenv";
+import crypto from "crypto";
+import fs from "fs";
+const { key, iv } = JSON.parse(fs.readFileSync('./encryption_key.json', 'utf8'));
+const encryptedKeys = JSON.parse(fs.readFileSync('./bucket_keys.enc.json', 'utf8'));
 
-dotenv.config();
+const algorithm = 'aes-256-cbc';
+const encryptionKey = Buffer.from(key, 'hex');
+const ivBuffer = Buffer.from(iv, 'hex');
+
+function decrypt(encryptedData) {
+    const decipher = crypto.createDecipheriv(algorithm, encryptionKey, ivBuffer);
+    let decrypted = decipher.update(encryptedData, 'hex', 'utf8');
+    decrypted += decipher.final('utf8');
+    return decrypted;
+}
+
+const AWS_ACCESS_KEY_ID = decrypt(encryptedKeys.encryptedAccess.encryptedData);
+const AWS_SECRET_ACCESS_KEY = decrypt(encryptedKeys.encryptedSecret.encryptedData);
+
 
 const s3 = new S3Client({
     region: process.env.AWS_REGION,
     credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+        accessKeyId: AWS_ACCESS_KEY_ID,
+        secretAccessKey: AWS_SECRET_ACCESS_KEY,
     }
 });
 
